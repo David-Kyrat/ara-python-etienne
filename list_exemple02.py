527809facfd861891ed2ebf8ecc1178a11976e2b
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2023-10-05 19:07:20 +0200

    Update

--- list_exemple02.py
+++ list_exemple02.py
@@ -24,9 +24,49 @@ print(s)  # 89
 print("---------------------------\n")
 
 
-
 print(range(5))
 # print(list(range(5)))
 print(liste)
 
 
+
+# =============================================
+#  Exemple: for idx in range(N)
+# =============================================
+
+print("---------------------------\n")
+print("Exemple for idx in range(N): \n")
+
+
+liste = [12, 13, 42, 76, 89]
+
+for idx in range(5):
+    print(idx)
+
+print("---\n")
+
+for idx in range(5):
+    s = liste[idx]
+    print(s)
+
+print("---\n")
+
+s = 0
+for idx in range(5):
+    s += liste[idx]
+    print(s)
+
+
+# =============================================
+#  Exemple: for idx in range(N)
+# =============================================
+
+print("---------------------------\n")
+print("Exemple for x in liste: \n")
+
+
+liste = [12, 13, 42, 76, 89]
+s = 0
+for elem in liste:
+    s += elem
+    print(s)
